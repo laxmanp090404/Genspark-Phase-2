71a0399ca54fa72487a03f6da1065e57cede6c38
Laxman P <theofficiallaxmanp@gmail.com>
2026-06-25 15:12:37 +0530

    Docker and Kubernetes

---
+++ DOCKER/Day3/compose-lab/compose-postgres-api-lab/CustomerApi/Data/CustomerDbContext.cs
@@ -0,0 +1,11 @@
+using CustomerApi.Models;
+using Microsoft.EntityFrameworkCore;
+namespace CustomerApi.Data;
+public class CustomerDbContext : DbContext
+{
+public CustomerDbContext(DbContextOptions<CustomerDbContext> options)
+: base(options)
+{
+}
+public DbSet<Customer> Customers => Set<Customer>();
+}
